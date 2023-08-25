3f58197171d77f8c0e9c777723a2535e5d3f678d
Aaron Pang <pang.aaron56@gmail.com>
2023-08-25 00:03:28 -0400

    Initial Commit

---
+++ CitizenInterview/CitizenInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitizenInterviewApp.swift
+//  CitizenInterview
+//
+//  Created by Aaron Pang on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CitizenInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
