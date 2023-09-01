f20ea11e65e29e64bcc3d83c1cf9db52c3f8982c
Aaron Pang <pang.aaron56@gmail.com>
2023-09-01 01:07:11 -0400

    Add hyphens

--- CitizenInterview/ContentView.swift
+++ CitizenInterview/ContentView.swift
@@ -12,56 +12,72 @@ struct ContentView: View {
     @State private var question = ""
     @State private var answer = ""
     @State private var questionCounter = 0
-    
-    @State private var questions : [QuestionModel] = []
-    
+
+    @State private var questions: [QuestionModel] = []
+
     init() {
         self._questions = State(initialValue: parseJSONFile(forName: "Questions")!)
         let question1 = questions[0]
-        self._question = State(initialValue: question1.question )
-        self._answer = State(initialValue: question1.answer[0] )
+        self._question = State(initialValue: question1.question)
+        self._answer = State(initialValue: question1.answers[0])
     }
-    
+
     var body: some View {
         VStack(alignment: .leading) {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-                .onTapGesture {
+            Text(String(format: "Question #%d", questionCounter + 1)).fontWeight(.bold)
+            Text(question).padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+            if showAnswer {
+                Text(answer)
+                Button("Next Question") {
+                    showAnswer.toggle()
+                    questionCounter += 1
+                    if questionCounter >= questions.count {
+                        questionCounter = 0
+                    }
+                    question = questions[questionCounter].question
+
+                    // Update the answer
+                    answer = parseAnswersIntoString(answers: questions[questionCounter].answers)
+                }
+            } else {
+                Button("Show Answer") {
                     showAnswer.toggle()
                 }
-            Text(question)
-            Text(answer).opacity(showAnswer ? 1 : 0)
-            Image(systemName: "globe").opacity(showAnswer ? 1 : 0).onTapGesture {
-                showAnswer.toggle()
-                questionCounter+=1
-                question = questions[questionCounter].question
             }
         }
         .padding()
-        .background(Color.purple)
+        .background(Color.white)
         .frame(maxWidth: .infinity, // Full Screen Width
-                                        maxHeight: .infinity, // Full Screen Height
-                                        alignment: .topLeading)
+               maxHeight: .infinity, // Full Screen Height
+               alignment: .topLeading)
     }
-    
+
+    private func parseAnswersIntoString(answers: [String]) -> String {
+        var answerString = ""
+        let appendHyphen = answers.count > 1
+        answers.forEach { answer in
+            answerString.append((appendHyphen ? " - " : "") + answer + "\n")
+        }
+        return answerString
+    }
+
     func parseJSONFile(forName name: String) -> [QuestionModel]? {
-       do {
-          if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"),
-             let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
-              do {
-                  let questions: [QuestionModel] = try JSONDecoder().decode([QuestionModel].self, from: jsonData)
-                  return questions
-              }
-              catch {
-                  print(error)
-              }
-          }
-           return nil
-       } catch {
-          print(error)
-           return nil
-       }
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"),
+               let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8)
+            {
+                do {
+                    let questions: [QuestionModel] = try JSONDecoder().decode([QuestionModel].self, from: jsonData)
+                    return questions
+                } catch {
+                    print(error)
+                }
+            }
+            return nil
+        } catch {
+            print(error)
+            return nil
+        }
     }
 }
 
--- CitizenInterview/QuestionModel.swift
+++ CitizenInterview/QuestionModel.swift
@@ -9,5 +9,5 @@ import Foundation
 
 struct QuestionModel : Decodable{
     let question : String
-    let answer : Array<String>
+    let answers : Array<String>
 }
