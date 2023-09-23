9028b004fe613d2681f4c680c14b0d40820cfcfb
Aaron Pang <pang.aaron56@gmail.com>
2023-09-23 19:46:13 -0400

    Location first run

--- CitizenInterview/MainMenuView.swift
+++ CitizenInterview/MainMenuView.swift
@@ -8,6 +8,12 @@
 import CoreLocation
 import SwiftUI
 
+enum LocationAuthorizationViewState {
+    case showAddress
+    case showRequestButton
+    case showStatePicker
+}
+
 enum OfficialRole: String, Codable {
     case none
     case president
@@ -46,53 +52,59 @@ struct MainMenuView: View {
     @State private var isAbove65 = false
     @State private var locationEnabled = false
     @State private var answerModel: DynamicAnswerResultsModel?
-    private let locationDelegateHandler = MainMenuViewDelegateHandler()
-    private let locationManager = CLLocationManager()
-
-    private var address: String = "100 sendera lane fort worth"
+    @StateObject var locationManager = LocationManager()
+    @State private var locationAuthorizationViewState = LocationAuthorizationViewState.showRequestButton
 
     enum AmericanState: String, CaseIterable, Identifiable {
         case alabama, alaska, arizona, arkansas, california, colorado, connecticut, delaware, florida, georgia, hawaii, idaho, illinois, indiana, iowa, kansas, kentucky, louisiana, maine, maryland, massachusetts, michigan, minnesota, mississippi, missouri, montana, nebraska, nevada, new_hampshire, new_jersey, new_mexico, new_york, north_carolina, north_dakota, ohio, oklahoma, oregon, pennsylvania, rhode_island, south_carolina, south_dakota, tennessee, texas, utah, vermont, virginia, washington, west_virginia, wisconsin, wyoming
         var id: Self { self }
     }
 
-    init() {
-        // Check if location is enabled, if it is, then cool!
-        switch locationManager.authorizationStatus {
-        case .notDetermined: break
-        // Show authorization button
-        case .denied: break
-        case .restricted: break
-        // Show picker and button to screen to allow authorization
-        case .authorizedAlways: break
-        case .authorizedWhenInUse: break
-            // Sweet do nothing
-        }
-        // If location is not enabled, then show picker where we will do the fetching later and a dialog more info button to change settings and allow data fetching
-//        locationEnabled = UserDefaults.standard.bool(forKey: "location_enabled")
-    }
-
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading) {
-                Text("Please accept the location access so we can provide to you the most accurate information for your studies")
-                Toggle("Location Enabled", isOn: $locationEnabled)
-                    .toggleStyle(.switch)
-                    .frame(alignment: .bottom).padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
-                    .tint(Color(UIColor.systemBlue))
-                    .onChange(of: locationEnabled) { _ in
-                        let locationManager = CLLocationManager()
-                        locationManager.delegate = locationDelegateHandler
-                    }
+                Text("Please accept the location access so we can provide to you the most accurate information for your studies").onAppear {
+                    // Check if location is enabled on appear
+                    // Depending on permissions, show different UIs (request button, address, or state picker)
+                    switch locationManager.manager.authorizationStatus {
+                    case .notDetermined: locationAuthorizationViewState = .showRequestButton
+                    // Show authorization button
+                    case .denied: locationAuthorizationViewState = .showStatePicker
+                    case .restricted: locationAuthorizationViewState = .showStatePicker
+                    // Show picker and button to screen to allow authorization
+                    case .authorizedAlways:
+                        locationAuthorizationViewState = .showAddress
+                        locationManager.manager.requestLocation()
 
-//                Picker("Pick Your State", selection: $selectedState, content: {
-//                    ForEach(AmericanState.allCases) { americanState in
-//                        Text(americanState.rawValue.capitalized.replacingOccurrences(of: "_", with: " "))
-//                    }
-//                })
-//                .pickerStyle(.wheel)
-//                .onChange(of: selectedState) { _ in
-//                }
+                    case .authorizedWhenInUse:
+                        locationAuthorizationViewState = .showAddress
+                        // Sweet get the user location and see if it needs to be stored
+                        locationManager.manager.requestLocation()
+                    @unknown default:
+                        break
+                    }
+                }
+                if locationAuthorizationViewState == .showAddress {
+                    Text(String(format:"The address we are using is:\n%@", locationManager.location))
+                } else if locationAuthorizationViewState == .showRequestButton {
+                    Toggle("Location Enabled", isOn: $locationEnabled)
+                        .toggleStyle(.switch)
+                        .frame(alignment: .bottom).padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+                        .tint(Color(UIColor.systemBlue))
+                        .onChange(of: locationEnabled) { _ in
+                            locationManager.manager.requestWhenInUseAuthorization()
+                        }
+                } else {
+                    Picker("Pick Your State", selection: $selectedState, content: {
+                        ForEach(AmericanState.allCases) { americanState in
+                            Text(americanState.rawValue.capitalized.replacingOccurrences(of: "_", with: " "))
+                        }
+                    })
+                    .pickerStyle(.wheel)
+                    .onChange(of: selectedState) { _ in
+                    }
+                }
+
                 // If the user declines the location, then show a picker for them to pick the state
                 Toggle("Above the age of 65?", isOn: $isAbove65)
                     .toggleStyle(.switch)
@@ -136,7 +148,7 @@ struct MainMenuView: View {
         // Check if the data is cached with a refresh date of < 2 months, then fetch the info
         var urlComponents = URLComponents(string: "https://www.googleapis.com/civicinfo/v2/representatives")!
         urlComponents.queryItems = [
-            URLQueryItem(name: "address", value: address),
+            URLQueryItem(name: "address", value: locationManager.location),
             URLQueryItem(name: "key", value: "AIzaSyCOGbH_RzRwROVMroT7EFklR9sVpIj43Y4")
         ]
         let url = urlComponents.url!
@@ -198,7 +210,7 @@ struct MainMenuView: View {
             official.name
         }
         if names.count <= 0 {
-            return [String(format: "There are no %@s for the address: %@", role.rawValue, address)]
+            return [String(format: "There are no %@s for the address: %@", role.rawValue, locationManager.location)]
         }
         return names
     }
@@ -225,8 +237,22 @@ struct MainMenuView: View {
     }
 }
 
-class MainMenuViewDelegateHandler: NSObject, CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {}
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var location: String = ""
+
+    var manager = {
+        let manager = CLLocationManager()
+        return manager
+    }()
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = "3 Court Square West, NY"
+    }
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {}
 }
